c2d127af0d05bb59ede0395bdc0f74bb2bd139cb
Andreas Pehnack <andreas@synalysis.com>
2024-08-29 22:21:37 +0200

    elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Nex.MixProject do
     [
       app: :nex,
       version: "0.1.0",
-      elixir: "~> 1.17",
+      elixir: "~> 1.16",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       name: "Nex",
